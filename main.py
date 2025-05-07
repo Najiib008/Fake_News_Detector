86e2ebea69fac33467a38e0f7e1d10e128533315
mohamed Abdi Hassan <123385057+mahamad008@users.noreply.github.com>
2025-05-07 01:09:43 -0700

    some changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import re
 import string
-
+# 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
